b031e4d755bb8a31bc18f38fd6dbadc8d9a5964c
rob tillaart <rob.tillaart@gmail.com>
2021-01-09 12:25:46 +0100

    add Arduino-CI

---
+++ test/unit_test_001.cpp
@@ -0,0 +1,71 @@
+//
+//    FILE: unit_test_001.cpp
+//  AUTHOR: Rob Tillaart
+//    DATE: 2021-01-09
+// PURPOSE: unit tests for the timing libraty
+//          https://github.com/RobTillaart/timing
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// ----------------------------
+// assertEqual(expected, actual);               // a == b
+// assertNotEqual(unwanted, actual);            // a != b
+// assertComparativeEquivalent(expected, actual);    // abs(a - b) == 0 or (!(a > b) && !(a < b))
+// assertComparativeNotEquivalent(unwanted, actual); // abs(a - b) > 0  or ((a > b) || (a < b))
+// assertLess(upperBound, actual);              // a < b
+// assertMore(lowerBound, actual);              // a > b
+// assertLessOrEqual(upperBound, actual);       // a <= b
+// assertMoreOrEqual(lowerBound, actual);       // a >= b
+// assertTrue(actual);
+// assertFalse(actual);
+// assertNull(actual);
+
+// // special cases for floats
+// assertEqualFloat(expected, actual, epsilon);    // fabs(a - b) <= epsilon
+// assertNotEqualFloat(unwanted, actual, epsilon); // fabs(a - b) >= epsilon
+// assertInfinity(actual);                         // isinf(a)
+// assertNotInfinity(actual);                      // !isinf(a)
+// assertNAN(arg);                                 // isnan(a)
+// assertNotNAN(arg);                              // !isnan(a)
+
+#include <ArduinoUnitTests.h>
+
+#include "Arduino.h"
+#include "timing.h"
+
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+
+unittest(test_constructor)
+{
+  fprintf(stderr, "VERSION: %s\n", TIMING_LIB_VERSION);
+  
+  microSeconds mic();
+  assertEqual(0. mic.getOffset());
+  mic.set(100);
+  assertEqual(100. mic.getOffset());
+
+  milliSeconds mil();
+  assertEqual(0. mil.getOffset());
+  mil.set(100);
+  assertEqual(100. mil.getOffset());
+
+  seconds sec();
+  assertEqual(0. sec.getOffset());
+  sec.set(100);
+  assertEqual(100. sec.getOffset());
+
+  assertEqual(1, 1);
+}
+
+unittest_main()
+
+// --------
--- timing.h
+++ timing.h
@@ -6,18 +6,15 @@
 // PURPOSE: Arduino library with wrapper classes for seconds millis micros 
 //     URL: https://github.com/RobTillaart/timing
 //
-// HISTORY: 
-// 0.1.00 - 2011-01-04 initial version
-// 0.1.01 - 2011-07-19 lib version
-// 0.1.02 - 2015-03-02 move all to mymillis.h file so compiler can optimize
-// 0.2.0    2020-07-07 renamed to timing.h
+//  HISTORY: 
+//  0.1.00  2011-01-04 initial version
+//  0.1.01  2011-07-19 lib version
+//  0.1.02  2015-03-02 move all to mymillis.h file so compiler can optimize
+//  0.2.0   2020-07-07 renamed to timing.h
+//  0.2.1   2021-01-09 added Arduino-CI + unit test
 
-// IDEAS
-//  char * toClock(); 
-// seconds.toClock() -> DD 12:34:56
-// milliSeconds.toClock() -> 12:23:45.123
-// microSeconds.toCLock() -> ???
 
+#define TIMING_LIB_VERSION        (F("0.2.1"))
 
 class microSeconds
 {
@@ -25,6 +22,7 @@ public:
   microSeconds()                  { set(0); }
   uint32_t now()                  { return micros() - _offset; }
   void set(uint32_t val = 0UL)    { _offset = micros() - val; }
+  uint32_t getOffset()            { return _offset; };
 
 private:
   uint32_t _offset = 0UL;
@@ -37,6 +35,7 @@ public:
   milliSeconds()                  { set(0); };
   uint32_t now()                  { return millis() - _offset; };
   void set(uint32_t val = 0UL)    { _offset = millis() - val; };
+  uint32_t getOffset()            { return _offset; };
 
 private:
   uint32_t _offset = 0UL;
@@ -49,6 +48,7 @@ public:
   seconds()                       { set(0); }
   uint32_t now()                  { return millis()/1000UL - _offset; }
   void set(uint32_t val = 0UL)    { _offset = millis()/1000UL - val; }
+  uint32_t getOffset()            { return _offset; };
 
 private:
     uint32_t _offset = 0UL;
