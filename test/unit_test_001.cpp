dd70c30d4e72d718376ccc69903f43ddc1c8d982
rob tillaart <rob.tillaart@gmail.com>
2021-01-09 12:54:40 +0100

    add Arduino-CI

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -51,17 +51,17 @@ unittest(test_constructor)
   microSeconds mic;
   assertEqual(0, mic.getOffset());
   mic.set(100);
-  assertEqual(100, mic.getOffset());
+  assertEqual(4294967196, mic.getOffset());  // max uint32_t - 100
 
   milliSeconds mil;
   assertEqual(0, mil.getOffset());
   mil.set(100);
-  assertEqual(100, mil.getOffset());
+  assertEqual(4294967196, mil.getOffset());
 
   seconds sec;
   assertEqual(0, sec.getOffset());
   sec.set(100);
-  assertEqual(100, sec.getOffset());
+  assertEqual(4294967196, sec.getOffset());
 
   fprintf(stderr, "%d\n", mic.now());
   fprintf(stderr, "%d\n", mil.now());
